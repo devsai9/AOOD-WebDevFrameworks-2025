075c14780b8c9bb2542d1afe16ef68a389b4de62
devsai9 <63507117+devsai9@users.noreply.github.com>
2025-04-03 16:44:36 -0400

    stuff

--- 5.14/script.js
+++ 5.14/script.js
@@ -1,28 +1,46 @@
-class Example extends Phaser.Scene {
+class Game extends Phaser.Scene {
     preload() {
         // this.load.setBaseURL('https://cdn.phaserfiles.com/v385');
+        this.load.setBaseURL("/")
 
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
+        this.load.atlas("player", "assets/player/Player/Player.png", "assets/player/Player/Player.json");
+        this.load.atlas("floor", "assets/floor/Floor/Floor.png", "assets/floor/Floor/Floor.json");
     }
 
     create() {
-        this.add.image(400, 300, 'sky');
-
-        const particles = this.add.particles(0, 0, 'red', {
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
+        const player = this.physics.add.sprite(100, 100, "player");
+        player.setScale(2);
+        player.setCollideWorldBounds(true);
+        player.anims.create({
+            key: "default",
+            frames: this.anims.generateFrameNames("player", {
+                prefix: "Player",
+                suffix: ".png",
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 4,
+            repeat: -1
         });
+        player.play("default");
+        this.player = player;
 
-        const logo = this.physics.add.image(400, 100, 'logo');
-
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
-
-        particles.startFollow(logo);
+        const fl = this.add.tileSprite(0, (600-(32*2)), 800, 32, 'floor');
+        fl.originX = 0;
+        fl.originY = 0;
+        fl.setScale(2);
+        fl.anims.create({
+            key: "default",
+            frames: this.anims.generateFrameNames("floor", {
+                prefix: "Seaweed Floor",
+                suffix: ".png",
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 4,
+            repeat: -1
+        });
+        fl.play("default");
     }
 }
 
@@ -30,13 +48,14 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: Example,
+    scene: Game,
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 200 }
         }
-    }
+    },
+    pixelArt: true,
 };
 
 const game = new Phaser.Game(config);
